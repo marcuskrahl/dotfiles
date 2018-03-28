93e007c2477719cfd7d672b05b929a57fca24418
Marcus Krahl <mail@marcuskrahl.de>
2018-03-28 11:34:42 +0200

    added neovim and auto install plugins

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
+(
+set -x;
 dir=`pwd`;
 if [ ! -f ~/.tmux.conf ]; then; ln -s $dir/tmux/tmux.conf ~/.tmux.conf; fi
 if [ ! -f ~/.vimrc ]; then; ln -s $dir/vim/vimrc ~/.vimrc; fi
 if [ ! -f ~/.zshrc ]; then; ln -s $dir/zsh/zshrc ~/.zshrc; fi
 if [ ! -f ~/.zprofile ]; then; ln -s $dir/zsh/zprofile ~/.zprofile; fi
+if [ ! -f ~/.config/nvim/init.vim ]; then; ln -s $dir/nvim/init.vim ~/.config/nvim/init.vim; fi
+)
