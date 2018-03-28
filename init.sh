6e5e74b7a468260bec432455961575fb1cddb7d8
Marcus Krahl <mail@marcuskrahl.de>
2018-03-28 10:51:18 +0200

    added zsh config files

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#!/bin/zsh
 dir=`pwd`;
-ln -s $dir/tmux/tmux.conf ~/.tmux.conf
-ln -s $dir/vim/vimrc ~/.vimrc
+if [ ! -f ~/.tmux.conf ]; then; ln -s $dir/tmux/tmux.conf ~/.tmux.conf; fi
+if [ ! -f ~/.vimrc ]; then; ln -s $dir/vim/vimrc ~/.vimrc; fi
+if [ ! -f ~/.zshrc ]; then; ln -s $dir/zsh/zshrc ~/.zshrc; fi
+if [ ! -f ~/.zprofile ]; then; ln -s $dir/zsh/zprofile ~/.zprofile; fi
